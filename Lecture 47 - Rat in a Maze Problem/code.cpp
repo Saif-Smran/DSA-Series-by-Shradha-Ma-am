10d1783c1bb2418d47e88f13e0799672c69c319f
A.H.M Saif Smran <23303079@iubat.edu>
2024-12-12 08:58:53 +0600

    12/12/2024

---
+++ Lecture 47 - Rat in a Maze Problem/code.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void helper(vector<vector<int>> &m, int r, int c, string path, vector<string> &ans)
+{
+    int n = m.size();
+    // Base Case
+    if (r < 0 || c < 0 || r >= n || c >= n || m[r][c] == 0)
+    {
+        return;
+    }
+    // Destination
+    if (r == n - 1 && c == n - 1)
+    {
+        ans.push_back(path);
+        return;
+    }
+
+    m[r][c] = 0;
+    // Recursive Case
+    helper(m, r + 1, c, path + "D", ans); // Down
+    helper(m, r, c - 1, path + "L", ans); // Left
+    helper(m, r, c + 1, path + "R", ans); // Right
+    helper(m, r - 1, c, path + "U", ans); // Up
+    m[r][c] = 1;
+}
+
+vector<string> findPath(vector<vector<int>> &m)
+{
+    vector<string> ans;
+    string path = "";
+
+    helper(m, 0, 0, path, ans);
+    return ans;
+}
+
+int main()
+{
+    vector<vector<int>> mat = {{1, 0, 0, 0},
+                               {1, 1, 0, 1},
+                               {1, 1, 0, 0},
+                               {0, 1, 1, 1}};
+    vector<string> res = findPath(mat);
+    for (string s : res)
+    {
+        cout << s << endl;
+    }
+
+    return 0;
+}
